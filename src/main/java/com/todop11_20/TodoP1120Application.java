0f610227d61c16aabb9c652238f9907ddb856fc6
TE <gimt75257@gmail.com>
2025-01-20 09:29:35 +0900

    create project, directory

---
+++ src/main/java/com/todop11_20/TodoP1120Application.java
@@ -0,0 +1,13 @@
+package com.todop11_20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoP1120Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TodoP1120Application.class, args);
+  }
+
+}
