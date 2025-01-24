cfabf4603c26b7b4b738b50aa8d85c81a9f77461
TE <gimt75257@gmail.com>
2025-01-24 10:00:52 +0900

    :bulb:update: log comment in code

--- src/main/java/com/todop11_20/common/aop/AnnotationLoggingAspect.java
+++ src/main/java/com/todop11_20/common/aop/AnnotationLoggingAspect.java
@@ -15,13 +15,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class AnnotationLoggingAspect {
 
-  private static final Logger logger= LoggerFactory.getLogger(AnnotationLoggingAspect.class);
+  private static final Logger logger= LoggerFactory.getLogger(AnnotationLoggingAspect.class); // 의존성 주입 필요!
 
   @Pointcut("@annotation(com.todop11_20.common.aop.LoggingAop)")
   private void loggingAopAnnotation() {}
 
   @Around("loggingAopAnnotation()")
   public Object annotaionAopTest(ProceedingJoinPoint joinPoint) {
+    logger.error("::: todo 조회 서비스 시작 :::");  // 로깅 config 에서 logger를 씀으로써 특정 클래스에 로그를 남길 때, log 대신 logger 를 쓴다
     long startTime = System.currentTimeMillis();
 
     try {
@@ -33,7 +34,8 @@ public class AnnotationLoggingAspect {
     } finally {
       long endTime = System.currentTimeMillis();
       long excutionTime = endTime - startTime;
-      logger.info("::: ExcutionTime : {}ms",excutionTime);
+      logger.error("::: todo 조회 서비스 종료 :::");
+      logger.info("::: todo 조회 서비스 ExcutionTime : {}ms :::",excutionTime);
       return null;
     }
 
